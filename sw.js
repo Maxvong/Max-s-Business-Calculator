4b8e80f20920b82fb5f842566f4239eaec14c32e
Maxvong <vong.mm20@gmail.com>
2026-09-23 12:31:45 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,39 @@
-self.addEventListener('fetch', function(event) {
-  // 这是通行证代码，专门用来让安卓允许安装 APP
+const CACHE_NAME = 'max-business-calculator-v1';
+
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('./index.html');
+            })
+    );
 });
